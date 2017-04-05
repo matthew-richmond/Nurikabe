03ef583263a6e8cb35246fb436b55e4ddf54bf4f
Matthew Richmond <mjr1408@rit.edu>
2017-04-04 23:50:19 -0400

    I hate this, truly. Know that.

--- src/NurikabeConfig.java
+++ src/NurikabeConfig.java
@@ -5,10 +5,9 @@ import jdk.nashorn.internal.runtime.regexp.joni.Config;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Collection;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Scanner;
+import java.io.PipedInputStream;
+import java.lang.reflect.Array;
+import java.util.*;
 
 /**
  * A class to represent a single configuration in the Nurikabe puzzle.
@@ -43,13 +42,10 @@ public class NurikabeConfig implements Configuration {
             this.rows = Integer.parseInt(rowcol[0]);
             this.cols = Integer.parseInt(rowcol[1]);
             this.board = new char[this.rows][this.cols];
-            while(!in.nextLine().equals(""))
+            for (int row = 0; row < this.rows; row++)
             {
-                for (int row = 0; row < this.rows; row++)
-                {
-                    char[] line = in.nextLine().trim().replaceAll(" ","").toCharArray();
-                    this.board[row] = line;
-                }
+                char[] line = in.nextLine().trim().replaceAll(" ","").toCharArray();
+                this.board[row] = line;
             }
         }
     }
@@ -63,10 +59,17 @@ public class NurikabeConfig implements Configuration {
 
     protected NurikabeConfig(NurikabeConfig other)
     {
-        this.seas = other.seas;
-        this.rows = other.rows;
-        this.cols = other.cols;
-        this.board = other.board;
+        this.seas = other.getSeas();
+        this.rows = other.getRows();
+        this.cols = other.getCols();
+        this.board = new char[this.rows][this.cols];
+        for(int row = 0; row<this.rows; row++)
+        {
+            for(int col = 0; col<this.cols; col ++)
+            {
+                this.board[row][col] = other.board[row][col];
+            }
+        }
     }
 
     @Override
@@ -83,8 +86,8 @@ public class NurikabeConfig implements Configuration {
                 {
                     suc1.board[row][col] = '@';
                     suc1.seas += 1;
-                    suc2.board[row][col] = '#';
                     succ.add(suc1);
+                    suc2.board[row][col] = '#';
                     succ.add(suc2);
                     return succ;
                 }
@@ -98,28 +101,135 @@ public class NurikabeConfig implements Configuration {
     public boolean isValid()
     {
         boolean complete = true;
+        int empties = 0;
+        for(int r = 0; r<this.rows; r ++)
+        {
+            for(int c = 0; c<this.cols; c++)
+            {
+                if(this.board[r][c] == '.')
+                {
+                    complete = false;
+                }
+            }
+        }
         for(int row = 0; row<this.rows; row++) {
             for (int col = 0; col < this.cols; col++)
             {
                 char view = this.board[row][col];
                 if(view == '.')
                 {
-                    complete = false;
+                    empties++;
                 }
                 else if(Character.isDigit(view))
                 {
-                    int[] ref = {row,col};
-                    this.countchar('#', ref);
+                    String ref = row + " " +col;
+                    int num_island = Character.getNumericValue(view);
+                    if(complete)
+                    {
+                        if (!(this.countcharDFS('#', ref) == num_island)) {
+                            return false;
+                        }
+                    }
+                    else
+                        return(this.countcharDFS('#',ref) <= num_island);
+                }
+                else if(view == '@')
+                {
+
                 }
 
             }
+
+        }
+        if(complete)
+        {
+            String ref =  "";
+            for(int row = 0; row<this.rows; row++)
+            {
+                for (int col = 0; col < this.cols; col++)
+                {
+                    if(this.board[row][col] == '@')
+                    {
+                        ref = row + " " + col;
+                        if(row+1 < this.rows)
+                        {
+                            if(col+1 < this.cols)
+                            {
+                                if(this.board[row+1][col+1] == this.board[row][col])
+                                {
+                                    if(this.board[row+1][col] == this.board[row][col])
+                                    {
+                                        if(this.board[row][col+1] == this.board[row][col])
+                                        {
+                                            return false;
+                                        }
+                                    }
+                                }
+                            }
+                        }
+
+
+                    }
+                }
+            }
+            return (countcharDFS('@',ref) == this.seas);
+
         }
         return true;
     }
 
-    private int countchar(char sym, int[] ref)
+    private int countcharDFS(char sym, String ref)
     {
-        return 0;
+        Map<String,Character> map = new HashMap<>();
+        return countcharhelper(map,ref,sym).size();
+    }
+    private Map<int[], Character> countcharhelper(Map visited,String ref,char sym)
+    {
+        String[] refs = ref.split(" ");
+        int row = Integer.parseInt(refs[0]);
+        int col = Integer.parseInt(refs[1]);
+        boolean ints = false;
+        if(!visited.containsKey(ref));
+        {
+            if (sym == '#')
+            {
+                ints = true;
+            }
+            if((this.board[row][col] == sym) || (Character.isDigit(this.board[row][col]) && ints))
+            {
+                visited.put(ref,sym);
+                if(row - 1 >= 0)
+                {
+                    String up = (row-1) + " " + col;
+                    if(this.board[row-1][col] == sym && !visited.containsKey(up))
+                    {
+                        countcharhelper(visited, up, sym);
+                    }
+                }
+                if(row + 1 < this.rows)
+                {
+                    String down = (row+1) + " " + col;
+                    if(this.board[row+1][col] == sym && !visited.containsKey(down))
+                    {
+                        countcharhelper(visited, down, sym);
+                    }
+                }
+                if(col - 1 >= 0)
+                {
+                    String left = row + " " + (col-1);
+                    if(this.board[row][col-1] == sym && !visited.containsKey(left))
+                    countcharhelper(visited,left,sym);
+                }
+                if(col+1 <this.cols)
+                {
+                    String right = row + " " + (col+1);
+                    if(this.board[row][col+1] == sym && !visited.containsKey(right))
+                    countcharhelper(visited,right,sym);
+                }
+            }
+
+        }
+        return visited;
     }
 
     @Override
@@ -128,7 +238,7 @@ public class NurikabeConfig implements Configuration {
         {
             for(int row = 0; row<this.rows;row++)
             {
-                char[] line = new char[this.cols];
+                char[] line = this.board[row];
                 for(char x: line)
                 {
                     if(x == '.')
@@ -166,4 +276,9 @@ public class NurikabeConfig implements Configuration {
         }
         return ret.trim();
     }
+
+    public int getRows(){return rows;}
+    public int getCols(){return cols;}
+    public int getSeas(){return seas;}
+    public char[][] getBoard(){return board;}
 }
